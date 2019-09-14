4121c3ba04579a48c9c1fc393944d4218417fa93
Jaime <jaime.escoto@hotmail.com>
2019-09-14 17:55:41 -0600

    Agregando Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+## Tasks Consola App (TCA)
+
+Esta es una aplicación CRUD para tareas en consola usando yargs.
+...
+Ejecutar este comando:
+```
+npm install
+```
--- config/yargs.js
+++ config/yargs.js
@@ -1,32 +1,24 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea'
+
+}
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente la tares'
+}
 const argv = require('yargs')
     .command('crear', 'Crea una tarea por hacer', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea'
-
-        }
+        descripcion
     }).command('actualizar', 'Actualiza el estado de una tarea', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea'
-
-        },
-        completado: {
-            default: true,
-            alias: 'c',
-            desc: 'Marca como completado o pendiente la tares'
-        }
+        descripcion,
+        completado
 
     })
     .command('borrar', 'Borra una tarea de la bd', {
-        descripcion: {
-            demand: true,
-            alias: 'd',
-            desc: 'Descripción de la tarea'
-
-        },
+        descripcion
     })
     .help()
     .argv;
