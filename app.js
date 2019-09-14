732e8030c460b21e4c7c3d8a5bdaa755d301386c
Jaime <jaime.escoto@hotmail.com>
2019-09-14 15:32:30 -0600

    Agregando funcion crear

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //const argv = require('yargs').argv;
 const argv = require('./config/yargs').argv;
-console.log(argv);
+
+const porHacer = require('./todo/todo');
 
 let comando = argv._[0];
 switch (comando) {
     case 'crear':
         console.log('Creando tarea');
-
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
         console.log('Mostrando todas las tareas por hacer');
